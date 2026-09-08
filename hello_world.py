4f92872162132e62a9d28dc7cddd3af665afe6f5
Nick <46430020+nalopes44@users.noreply.github.com>
2026-09-08 17:36:56 -0600

    Fix capitalization in greeting message

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,2 @@
 user_name = input('Please enter your first name: ')
-print(f"hello {user_name}! I really hope you're having a great day!")
+print(f"Hello {user_name}! I really hope you're having a great day!")
