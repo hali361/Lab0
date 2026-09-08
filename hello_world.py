771429c21ced5f00b33305e584380870c8238681
Hamza Ali <hali361@mtroyal.ca>
2026-09-08 16:31:39 -0600

    Remove merge conflict markers

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-print("hello world")
-=======
 user_name = input('Please enter your name: ')
 print(f"hello {user_name}! I really hope you're having a great day!")
->>>>>>> 05172d4 (Fixed hello world program)
