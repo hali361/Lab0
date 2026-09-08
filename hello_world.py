269f14d862040d3c84ea7dc08085f32ee16c20d4
Hamza Ali <hali361@mtroyal.ca>
2026-09-08 16:21:34 -0600

    Fixed hello world program

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 print("hello world")
+=======
+user_name = input('Please enter your name: ')
+print(f"hello {user_name}! Hope you're having a great day!")
+>>>>>>> 05172d4 (Fixed hello world program)
