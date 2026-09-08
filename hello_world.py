10f6ee9ac0850124b360392f946bba04e9dd79d3
hali361 <hali361@mtroyal.ca>
2026-09-08 16:28:29 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,5 +2,5 @@
 print("hello world")
 =======
 user_name = input('Please enter your name: ')
-print(f"hello {user_name}! Hope you're having a great day!")
+print(f"hello {user_name}! I really hope you're having a great day!")
 >>>>>>> 05172d4 (Fixed hello world program)
